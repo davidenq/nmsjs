35dadd84f6ea9d096de6ce3b45e8fd7a597951da
davidenq <david.nunez.dev@gmail.com>
2016-08-05 16:28:30 -0500

    ipc process for communicated with vuejs

---
+++ app/utils/ipc-snmp.js
@@ -0,0 +1,48 @@
+'use strict';
+const fs = require('fs');
+const Prims = require('prismjs');
+const {ipcMain} = require('electron');
+const commands = require('../config/commands');
+const ChildProcess = require('./child-process');
+const cp = new ChildProcess();
+let errs = [];
+// Recibe mensajes desde render process
+ipcMain.on('asynchronous-message', function(event, data) {
+
+    const commandAll = data.command.split(" ");
+    const command = commandAll[0];
+
+    if (commands.allowed.indexOf(command) == -1) {
+        conn.emit('response', {
+            outcome: 'Comando no disponible'
+        });
+    } else {
+
+        const size = commandAll.length;
+        const args = [];
+
+        for (let i = 1; i < size; i++) {
+            args.push(commandAll[i]);
+        }
+
+        cp.exec(command, args, data.command, (err, stdout) => {
+
+            if (stdout) {
+                console.log(Prims.languages)
+                Prims.languages.css.equal =/=/i;
+                Prims.languages.css.time =/[0-9]*:[0-9]*:[0-9]*.[0-9]/i;
+                const html = Prims.highlight(stdout, Prims.languages.css);
+                //Envía respuesta a render process
+                console.log(stdout)
+                event.sender.send('asynchronous-reply', html);
+            } else {
+                errs.push(err);
+                console.log(errs[1]);
+                //Envía respuesta a render process
+                event.sender.send('asynchronous-reply', errs[1]);
+            }
+        });
+    }
+});
+
+    
